3c0fdd22a936588fc830204c1aa27cc8e8d0af3f
Diego Pinedo <studio.sherpa@gmail.com>
2019-07-05 19:49:03 +0100

    First commit.
    Prepare gradle with basic dependencies

---
+++ app/src/main/java/com/sherpastudio/jpmorganalbums/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sherpastudio.jpmorganalbums;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
